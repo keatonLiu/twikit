734b4176d7abf48dfd160ba3f064214cc0f6001f
liuxiaotong05 <liuxiaotong05@kuaishou.com>
2025-09-28 17:46:30 +0800

    fix multiple cookies

--- twikit/client/client.py
+++ twikit/client/client.py
@@ -306,7 +306,7 @@ class Client:
         :class:`str`
             The CSRF token as a string.
         """
-        return self.http.cookies.get('ct0')
+        return self.http.cookies.get('ct0', domain='.x.com')
 
     @property
     def _base_headers(self) -> dict[str, str]:

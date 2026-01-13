0bec8e452adab3152e08ef539a08e14ed8eee2f6
keatonLiu <lxtxiaotong@foxmail.com>
2026-01-13 16:49:53 +0800

    fix header

--- twikit/client/v11.py
+++ twikit/client/v11.py
@@ -80,6 +80,7 @@ class V11Client:
 
         headers = {
             'x-guest-token': guest_token,
+            'user-agent': self.base._user_agent,
             'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
             'Referer': 'https://x.com/',
             'X-Twitter-Active-User': "yes",
